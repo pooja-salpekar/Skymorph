d64a8ee703fb0096c82026333c16baf90d3d9f08
Faris Mohammed <farismosman@gmail.com>
2013-06-22 11:27:38 +0300

    [Pooja/Faris] : Added secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-#Skymorph::Application.config.secret_token = '1466526df33dd70abaeb3dc100ae8129c53459d95345aa64d5c1903731a5f26d2add0d469a5bf8ec0adc580b6cefb96d1f8f09eb7d6a63216a267372c3fbbe86'
+Skymorph::Application.config.secret_token = 'cc34787dafbd72f08739657d548ff700aa5d1b2e45884cfe8e3f1864a890cdec5a4a916fe990e010e22fd28c365123bc8e4fea9fe9cded7ee87f93033942d0ae'
